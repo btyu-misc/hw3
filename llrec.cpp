c199a8f61961de6d8a6bc0b8735c350a2edd1022
Ben Yu <btyu@usc.edu>
2024-02-29 23:00:42 -0800

    FINAL edit for llrec, implemented head recursive design

--- llrec.cpp
+++ llrec.cpp
@@ -6,25 +6,25 @@
 //*********************************************
 
 
-void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot) {
+void pivotHelp(Node*& head, Node*& smaller, Node*& larger, int pivot) {
     if (head == NULL) {
         smaller = NULL;
         larger = NULL;
-        return;
     }
-    else if (head->val > pivot) {
-        //std::cout << head->val << "greater than " << pivot << std::endl;
-        llpivot(head->next, smaller, larger, pivot);
-        head->next = larger;
-        larger = head;
+    else if (head->val > pivot) { 
+        //std::cout << head->val << " greater than pivot " << pivot << std::endl;
+        larger = head; //since it's a direct reference it will change previouslarger->next
+        pivotHelp(head->next, smaller, larger->next, pivot);
     }
     else if (head->val <= pivot) {
-        //std::cout << head->val << "less than " << pivot << std::endl;
-        llpivot(head->next, smaller, larger, pivot);
-        head->next = smaller;
-        smaller = head; //WE'RE WORKING BACKWARDS BY USING HEAD AS A TEMP VALUE AND SETTING SMALLER TO HEAD AFTER WE SET HEAD'S NEXT
-        //VALUE. NEXT TIME WE'RE IN THIS ELSE LOOP THAT ONE'S NEXT BECOMES THIS VALUE AND SMALLER BECOMES THAT HEAD VALUE
+        //std::cout << head->val << " less than or equal to pivot " << pivot << std::endl;
+        smaller = head; 
+        pivotHelp(head->next, smaller->next, larger, pivot);
     }
+}
+
+void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot) {
+    pivotHelp(head, smaller, larger, pivot); 
     head = NULL;
 }
 
